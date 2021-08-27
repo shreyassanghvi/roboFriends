665825a6c6332a0a47d04d709daa5786e458f023
shreyassanghvi <shreyas.sanghvi11@gmail.com>
2021-08-27 15:51:56 +0530

    Build React App 2

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import "tachyons"
-import Card from "./Card"
+import CardList from "./CardList"
 import {robots} from "./robots";
 
 ReactDOM.render(
     <React.StrictMode>
-        <div>
-            <Card id={robots[0].id} name={robots[0].name} email = {robots[0].email}/>
-            <Card id={robots[1].id} name={robots[1].name} email = {robots[1].email}/>
-            <Card id={robots[2].id} name={robots[2].name} email = {robots[2].email}/>
-        </div>
-
+        <CardList robots ={robots}/>
     </React.StrictMode>,
     document.getElementById('root')
 );
